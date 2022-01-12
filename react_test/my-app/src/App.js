0eef1518925964aef521154fcab46b50d478339f
saidmukhamad <saidmukhamad@yandex.ru>
2022-01-13 01:28:15 +0300

    trash\musor

---
+++ react_test/my-app/src/App.js
@@ -0,0 +1,15 @@
+
+
+function App() {
+  return (
+    <div style={{width: "100%", textAlign: "center"}}>
+     
+      <h1 style={{margin: "0 auto"}}>
+        GOOOOOD MORNIIIING VIETNAAAAAM
+      </h1>
+       
+    </div>
+  );
+}
+
+export default App;
